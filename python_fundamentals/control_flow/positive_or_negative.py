112a17c0ea9929d3b6e153d1594057ce31dfb002
tanacohid <jeandbf5@gmail.com>
2026-09-01 10:35:46 +0200

    Fix syntax errors in positive_or_negative.py conditional statements

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -2,11 +2,11 @@
 
 number = __import__('random').randint(-10, 10)
 
-if number > 0
+if number > 0:
 	print(f"{number} is positive")
 
-elif number == 0
+elif number == 0:
 	print(f"{number} is zero")
 
-else
+else:
 	print(f"{number} is negative")
