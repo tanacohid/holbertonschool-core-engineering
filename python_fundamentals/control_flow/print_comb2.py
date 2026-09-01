c24b1343632a76534c28b5182e3c9b6f0d18f4c3
tanacohid <jeandbf5@gmail.com>
2026-09-01 14:55:14 +0200

    Fix indentation in print_comb2.py for consistent formatting

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 for i in range(100):
-	if i == 99:
-		print("{:02d}".format(i))
-	else:
-		print("{:02d}, ".format(i), end="")
+    if i == 99:
+        print("{:02d}".format(i))
+    else:
+        print("{:02d}, ".format(i), end="")
