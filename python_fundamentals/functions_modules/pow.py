67382527f92104d45ced80d7b3508967387a1101
tanacohid <jeandbf5@gmail.com>
2026-09-02 21:06:31 +0200

    Fix spacing and remove unnecessary blank lines in pow.py for consistency

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 def pow(a, b):
-    
+
     result = 1
-    
+
     for num in range(abs(b)):
         result *= a
-        
+
     if b < 0:
         return 1 / result
     return result
