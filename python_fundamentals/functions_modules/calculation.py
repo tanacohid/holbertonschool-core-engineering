c60ca27e37c01a096ba1e9d8677502efd5625d0d
tanacohid <jeandbf5@gmail.com>
2026-09-03 15:03:18 +0200

    Fix import statement in calculation.py to ensure proper function access

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+from calculation_1 import add, sub, mul, div
+
 if __name__ == "__main__":
     a = 10
     b = 5
-    from calculator_1 import add, sub, mul, div
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
