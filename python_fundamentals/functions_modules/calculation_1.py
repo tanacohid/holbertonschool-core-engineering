9bea43e8e3fba9f543f7c565f5fa98512df7545c
tanacohid <jeandbf5@gmail.com>
2026-09-03 14:41:09 +0200

    Refactor calculation_1.py to return results instead of printing, and update calculation.py to handle output

--- python_fundamentals/functions_modules/calculation_1.py
+++ python_fundamentals/functions_modules/calculation_1.py
@@ -3,13 +3,13 @@
 if __name__ == "__main__":
 
     def add(a, b):
-        print("{} + {} = {}".format(a, b, add(a, b)))
+        return a + b
 
     def sub(a, b):
-        print("{} - {} = {}".format(a, b, sub(a, b)))
+        return a - b
 
     def mul(a, b):
-        print("{} * {} = {}".format(a, b, mul(a, b)))
+        return a * b
 
     def div(a, b):
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        return a / b
