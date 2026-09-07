132ec726f583d57032b7e3657d20d431e043b848
tanacohid <jeandbf5@gmail.com>
2026-09-07 14:05:22 +0200

    Refactor safe_print_list_integers to improve integer printing and exception handling

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError, NameError):
-        return False
+    count = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+
+    print()
+    return count
