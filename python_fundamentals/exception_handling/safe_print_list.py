036244125ed605dc9d9fcecee9d622b90a3edfc5
tanacohid <jeandbf5@gmail.com>
2026-09-07 10:50:35 +0200

    Fix loop range and return value in safe_print_list to correctly count printed elements

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
 
-    for i in range(x - 1):
+    for i in range(x):
         try:
             print(my_list[i], end="")
+            count += 1
         except IndexError:
             break
 
     print()
-    return i
+    return count
