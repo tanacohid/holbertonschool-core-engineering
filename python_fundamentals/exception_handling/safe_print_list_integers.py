28ecfbe808853921478106acf790046a6b1e360a
tanacohid <jeandbf5@gmail.com>
2026-09-07 12:05:36 +0200

    Add safe_print_list_integers function for safe integer printing

---
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
