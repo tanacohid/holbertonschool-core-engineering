4c58cf7b8409068411527b69e352dc4f89521615
tanacohid <jeandbf5@gmail.com>
2026-09-08 14:50:03 +0200

    Add Square class with constructor and area method

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        return self._Square__size * self._Square__size
