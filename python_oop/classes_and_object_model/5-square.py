6978a408bdc5edf806956b3b087ebce0797b40a3
tanacohid <jeandbf5@gmail.com>
2026-09-08 18:58:11 +0200

    Remove position attribute and its validation from Square class

--- python_oop/classes_and_object_model/5-square.py
+++ python_oop/classes_and_object_model/5-square.py
@@ -5,19 +5,13 @@
 class Square:
     """Represents a square."""
 
-    def __init__(self, size=0, position(0, 0)):
+    def __init__(self, size=0):
         self.size = size
-        self.position = position
 
     @property
     def size(self):
         """Get the size of the square."""
         return self.__size
-
-    @property
-    def position(self):
-        """Get the position of the square."""
-        return self.__position
 
     @size.setter
     def size(self, value):
@@ -27,17 +21,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    @position.setter
-    def position(self, value):
-        """Set the position of the square."""
-        if type(value) is not tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) is not int or type(value[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     def area(self):
         """Return the area of the square."""
