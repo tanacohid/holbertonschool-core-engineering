448dd3c5d60c94ff26f1300a790d03359262387b
tanacohid <jeandbf5@gmail.com>
2026-09-08 17:56:37 +0200

    Implement Square class with size and position attributes, including validation

---
+++ python_oop/classes_and_object_model/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @property
+    def position(self):
+        """Get the position of the square."""
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        """Set the position of the square."""
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square with #."""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
+    def __str__(self):
+        return f"Square are {self.size} size"
