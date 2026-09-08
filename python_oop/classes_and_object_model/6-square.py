766281361709e6762344db959693bb060625437b
tanacohid <jeandbf5@gmail.com>
2026-09-08 18:45:56 +0200

    Fix syntax error in __init__ method of Square class by adding missing '=' in position parameter

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Represents a square."""
 
-    def __init__(self, size=0, position(0, 0)):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize a square."""
         self.size = size
         self.position = position
