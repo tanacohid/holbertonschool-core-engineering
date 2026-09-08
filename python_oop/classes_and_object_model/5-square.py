87b89c1c1103743ef9620174fa16c2e34787d074
tanacohid <jeandbf5@gmail.com>
2026-09-08 17:16:43 +0200

    Add __str__ method to Square class for string representation

--- python_oop/classes_and_object_model/5-square.py
+++ python_oop/classes_and_object_model/5-square.py
@@ -33,3 +33,5 @@ class Square:
         else:
             for i in range(self.__size):
                 print("#" * self.__size)
+    def __str__(self):
+        return f"Square are {self.size} size"
