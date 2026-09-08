6427136e02e5ab04d428c9c0e8b15b0bd94bee55
tanacohid <jeandbf5@gmail.com>
2026-09-08 16:25:27 +0200

    Refactor Square class constructor to use size property for validation

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -6,23 +6,22 @@ class Square:
     """Represents a square."""
 
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        return self.__size * self.__size
+        self.size = size
 
     @property
     def size(self):
+        """Get the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size of the square."""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
