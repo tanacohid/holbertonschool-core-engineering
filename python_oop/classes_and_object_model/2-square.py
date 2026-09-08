8b8a3522d79210442b7810cd4a5be713b98fdcd4
tanacohid <jeandbf5@gmail.com>
2026-09-08 14:33:21 +0200

    Refactor Square class constructor to restore type checks and exception handling

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -4,12 +4,10 @@
 
 class Square:
     """Represents a square."""
-    def __init__(self, size:int):
-        self.__size = size
 
-try:
-    carree = Square(int)
-except TypeError:
-    print("size must be an integer")
-except ValueError:
-    print("size must be >= 0")
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
