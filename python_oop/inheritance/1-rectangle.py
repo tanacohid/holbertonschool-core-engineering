d90701641d95d7cd27f98952fc94bcc9be20789d
tanacohid <jeandbf5@gmail.com>
2026-09-09 12:04:21 +0200

    Refactor Rectangle class to use BaseGeometry directly and remove redundant methods

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -2,19 +2,8 @@
 """Defines the BaseGeometry class."""
 
 
-class BaseGeometry:
-    """Represents the base geometry."""
+BaseGeometry = __import__("base_geometry").BaseGeometry
 
-    def area(self):
-        """Calculate the area."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate an integer."""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
     """Represents a rectangle."""
