847bba21eabb6233711148b7bfadd6530ad15405
tanacohid <jeandbf5@gmail.com>
2026-09-09 14:35:05 +0200

    Refactor integer validation in BaseGeometry to use f-strings for error messages

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Defines the BaseGeometry class."""
+
+
+BaseGeometry = __import__("base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle."""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__height}/{self.__width}"
+
