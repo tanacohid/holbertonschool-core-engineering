0425738e6c71bfec5c6661cea9a843f345c50b33
tanacohid <jeandbf5@gmail.com>
2026-09-09 11:25:02 +0200

    Fix indentation in BaseGeometry class methods

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -2,11 +2,11 @@
 
 class BaseGeometry:
 
-def area(self):
-    raise Exception("area() is not implemented")
+    def area(self):
+        raise Exception("area() is not implemented")
 
-def integer_validator(self, name, value)
-    if type(value) is not int:
-        raise ValueError("<name> must be an integer")
-    if value <= 0:
-        raise ValueError("<name> must be greater than 0")
+    def integer_validator(self, name, value)
+        if type(value) is not int:
+            raise ValueError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
