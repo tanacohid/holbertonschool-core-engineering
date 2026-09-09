b0d4d2556c9daa28b4437e06c297bc39225f348f
tanacohid <jeandbf5@gmail.com>
2026-09-09 11:45:42 +0200

    Implement BaseGeometry and Rectangle classes with validation methods

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Defines the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Represents the base geometry."""
+
+    def area(self):
+        """Calculate the area."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate an integer."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle."""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
