c39c51b60f42c15b99660060d04ae3d61f6a58d9
tanacohid <jeandbf5@gmail.com>
2026-09-21 14:14:01 +0200

    Fix module documentation and update write_file function to write text to a file

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-Ce module fournit la fonction read_file.
+Ce module fournit la fonction write_file.
 """
 
 
 def write_file(filename="", text=""):
-    """Lit un fichier texte codé en UTF-8 et l'affiche sur stdout."""
-    with open(filename, 'w', encoding="utf-8") as f:
-        data = f.filename
-        print(f.read(), end="")
-    return data
+    """Écrit une chaîne de caractères dans un fichier texte (UTF-8)
+
+    et renvoie le nombre de caractères écrits.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
