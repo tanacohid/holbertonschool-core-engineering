c7789035bee7235b4c539bf400d98f7fbef94aef
tanacohid <jeandbf5@gmail.com>
2026-09-21 13:37:52 +0200

    Add docstring to read_file function for better clarity on functionality

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+"""
+    Ce module contient une fonction qui lit un fichier texte
+    et affiche son contenu sur la sortie standard.
+    """
 
 def read_file(filename=""):
 
+    """
+    Lit un fichier texte (UTF-8) et l'affiche dans stdout.
+    """
     with open("UTF8.txt", 'r', encoding="utf-8") as f:
         data = f.read()
     print(data)
