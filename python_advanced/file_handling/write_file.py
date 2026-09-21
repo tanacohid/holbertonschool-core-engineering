27d6060d19856a04bf25bd5ae5376407e9eecc04
tanacohid <jeandbf5@gmail.com>
2026-09-21 14:14:14 +0200

    Remove unnecessary newline in write_file function docstring

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -6,7 +6,6 @@ Ce module fournit la fonction write_file.
 
 def write_file(filename="", text=""):
     """Écrit une chaîne de caractères dans un fichier texte (UTF-8)
-
     et renvoie le nombre de caractères écrits.
     """
     with open(filename, "w", encoding="utf-8") as f:
